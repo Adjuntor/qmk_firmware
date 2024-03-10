d23fe205b99a62494ef028624d67e3c57bd26842
Adjuntor <10227133+Adjuntor@users.noreply.github.com>
2024-03-10 15:37:14 -0300

    Update keymap.c

--- keyboards/keychron/c2_pro/ansi_v2/rgb/keymaps/adjuntor/keymap.c
+++ keyboards/keychron/c2_pro/ansi_v2/rgb/keymaps/adjuntor/keymap.c
@@ -23,6 +23,7 @@ enum layers{
   WIN_FN,
   WIN_NIKKE,
   WIN_MAPLE,
+  WIN_FORTNITE,
 };
 
 
@@ -30,11 +31,14 @@ enum custom_keycodes {
     STAB = SAFE_RANGE,
     ASSASSINATE,
     ALICEMACRO,
+    JAM,
 };
 
 bool spam_assassinate;
 bool spam_stab;
 bool spam_alicemacro;
+bool spam_jam = false;
+uint32_t spam_timer = 0;
 
 
 int randy(int max, int min){
@@ -80,6 +84,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
             spam_alicemacro = false;
         }
         break;
+
+    case JAM:
+        if (record->event.pressed) {
+            spam_jam = !spam_jam;
+        } else {
+            //nothing
+        }
+        break;
     }
     return true;
 };
@@ -95,6 +107,12 @@ void matrix_scan_user(void) {
   if (spam_alicemacro) {
     tap_code_delay(KC_MS_BTN1, randy(300,200));
     wait_ms(randy(50,20));
+  }
+  if (spam_jam) {
+    if (timer_elapsed32(spam_timer) > randy(150000,120000)) {
+        SEND_STRING(SS_DOWN(X_B) SS_DELAY(200) SS_TAP(X_SPACE) SS_DELAY(200) SS_UP(X_B));
+        spam_timer = timer_read32();
+    }
   }
 
 };
@@ -121,6 +139,9 @@ bool rgb_matrix_indicators_user(void) {
             break;
         case WIN_MAPLE:
             rgb_matrix_set_color_all(0xFF, 0x22, 0x00);  // RGB orange
+            break;
+        case WIN_FORTNITE:
+            rgb_matrix_set_color_all(0x5F, 0xCE, 0xEA);  // RGB sky blue
             break;
         default:
             break;
@@ -160,7 +181,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  TO(WIN_BASE),
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,                                _______,  _______,  _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  TG(WIN_FORTNITE),  RGB_SPD,  RGB_SAD,  _______,  _______,  _______,  _______,  _______,              _______,                                _______,  _______,  _______,
         _______,            _______,  _______,  _______,  _______,  NK_TOGG,  TG(WIN_NIKKE),  TG(WIN_MAPLE),  _______,  _______,  _______,  _______,            _______,            _______,  _______,  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,            _______         ),
 
@@ -179,4 +200,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                 KC_P4,    KC_P5,    KC_P6,
         STAB,               KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
         ASSASSINATE,        KC_LWIN,  KC_LALT,                      KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT         ),
+    
+    [WIN_FORTNITE] = LAYOUT_104_ansi(
+        KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_PSCR,  KC_NO,    TO(WIN_BASE),
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     JAM,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                 KC_P4,    KC_P5,    KC_P6,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT         ),
+
+
+
 };
