c8a4c0b1c831f573a033f4c8dd8c24d42191681e
Adjuntor <10227133+Adjuntor@users.noreply.github.com>
2024-04-16 11:22:59 -0300

    Update keymap.c

--- keyboards/keychron/c2_pro/ansi_v2/rgb/keymaps/adjuntor/keymap.c
+++ keyboards/keychron/c2_pro/ansi_v2/rgb/keymaps/adjuntor/keymap.c
@@ -28,10 +28,9 @@ enum layers{
 
 
 enum custom_keycodes {
-    ACTIVATE_INSERT = SAFE_RANGE,
+    ALICEMACRO = SAFE_RANGE,
     STAB,
     ASSASSINATE,
-    ALICEMACRO,
     JAM,
     LMOUSECLICK,
     
@@ -42,7 +41,6 @@ bool spam_stab;
 bool spam_alicemacro;
 bool spam_jam = false;
 bool spam_lmouseclick = false;
-bool activate_insert = false;
 uint32_t spam_timer = 0;
 
 
@@ -66,14 +64,6 @@ bool dip_switch_update_user(uint8_t index, bool active) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) {
-    case ACTIVATE_INSERT:
-        if (record->event.pressed) {
-            activate_insert = !activate_insert;
-            tap_code(KC_INSERT);
-        } else {
-            //nothing
-        }
-        break;
 
     case ASSASSINATE:
         if (record->event.pressed) {
@@ -163,7 +153,7 @@ void matrix_scan_user(void) {
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) { 
      for (uint8_t i = led_min; i <= led_max; i++) {
         if (host_keyboard_led_state().caps_lock) {
-            int keys[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 58};
+            int keys[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 37, 74, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 86, 101, 58, 30, 31, 32, 51, 52, 53};
             for(uint8_t r = 0; r < sizeof(keys)/sizeof(int); r++) {
                 rgb_matrix_set_color(keys[r], RGB_RED);
             }
@@ -171,20 +161,13 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         if (host_keyboard_led_state().num_lock) {
             int keys[] = {33, 34, 35, 36, 54, 55, 56, 57, 71, 72, 73, 87, 88, 89, 90, 102, 103};
             for(uint8_t r = 0; r < sizeof(keys)/sizeof(int); r++) {
-                rgb_matrix_set_color(keys[r], RGB_RED);
+                rgb_matrix_set_color(keys[r], RGB_WHITE);
             } 
         } else {
             int keys[] = {33, 34, 35, 36, 54, 55, 56, 57, 71, 72, 73, 87, 88, 89, 90, 102, 103};
             for(uint8_t r = 0; r < sizeof(keys)/sizeof(int); r++) {
                 rgb_matrix_set_color(keys[r], RGB_BLACK);
             }
-        }
-
-        if (activate_insert == true) {
-            int keys[] = {30, 31, 32, 51, 52, 53};
-            for(uint8_t r = 0; r < sizeof(keys)/sizeof(int); r++) {
-                rgb_matrix_set_color(keys[r], RGB_RED);
-            } 
         }
  
         if (spam_jam == true) {
@@ -254,7 +237,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_BASE] = LAYOUT_104_ansi(
         KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_PSCR,  RGB_TOG,    RGB_MOD,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  ACTIVATE_INSERT,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                 KC_P4,    KC_P5,    KC_P6,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
