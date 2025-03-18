98c8eabf834718948d94fb7f3f569993cfd53c55
Adjuntor <10227133+Adjuntor@users.noreply.github.com>
2025-03-18 03:36:54 -0300

    Update keymap.c

--- keyboards/keychron/k2_he/ansi_rgb/keymaps/adjuntor/keymap.c
+++ keyboards/keychron/k2_he/ansi_rgb/keymaps/adjuntor/keymap.c
@@ -22,7 +22,8 @@ enum layers {
     MAC_FN,
     WIN_BASE,
     WIN_FN,
-    NIKKE,
+    MACRO,
+
 };
 
 enum custom_keycodes {
@@ -30,7 +31,8 @@ enum custom_keycodes {
     SNIPERCOOP,
     ADVISE,
     LMOUSECLICK,
-    RMOUSECLICK
+    RMOUSECLICK,
+    GOBACK
 };
 
 bool spam_snipermacro  = false;
@@ -107,13 +109,13 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         switch(get_highest_layer(layer_state|default_layer_state)) {
 
         case WIN_BASE:
-            rgb_matrix_set_color(i, 0x00, 0x00, 0x00);  // RGB off
+            rgb_matrix_set_color(i, 0x00, 0x00, 0x00);  // RGB Off
             break;
         case WIN_FN:
-            rgb_matrix_set_color(i, 0x00, 0xFF, 0x00);  // RGB Green
+            rgb_matrix_set_color(i, 0x15, 0x80, 0x15);  // RGB Green
             break;
-        case NIKKE:
-            rgb_matrix_set_color(i, 0xF0, 0x14, 0x3C);  // RGB Crimson
+        case MACRO:
+            rgb_matrix_set_color(i, 0x8B, 0x45, 0x13);  // RGB Saddle Brown
             break;
         default:
             break;
@@ -141,7 +143,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______),
 
 [WIN_BASE] = LAYOUT_ansi_84(
-     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   RGB_MOD,
+     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_DEL,   TG(MACRO),
      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
      KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
@@ -149,16 +151,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT, MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [WIN_FN] = LAYOUT_ansi_84(
-     _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  RGB_TOG,
+     _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  _______,
      _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
      RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
      _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            _______,
-     _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  TG(NIKKE),  _______,  _______,  _______,          _______,  _______,  _______,
+     _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,          _______,  _______,  _______,
      _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______),
 
-
-[NIKKE] = LAYOUT_ansi_84(
-     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  TO(WIN_BASE),
+[MACRO] = LAYOUT_ansi_84(
+     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  GOBACK,
      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            ADVISE,
      _______,  _______,  _______,  _______,  SNIPERMACRO,  SNIPERCOOP,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,     LMOUSECLICK,
      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,            RMOUSECLICK,
@@ -215,6 +216,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             //nothing
         }
         break;
+
+    case GOBACK:
+        if (record->event.pressed) {
+            spam_snipermacro  = false;
+	    spam_snipercoop  = false;
+	    spam_advise = false;
+	    spam_lmouseclick = false;
+	    spam_rmouseclick = false;
+	    spam_timer = timer_read32();
+            //layer_invert(MACRO);
+        } else {
+            layer_move(WIN_BASE);
+            //nothing
+        }
+        break;
+
     }
 
     if (!process_record_keychron_common(keycode, record)) {
